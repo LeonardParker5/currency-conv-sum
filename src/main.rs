82001e5a190a5e3d6ff15fa1311becf4553e9e4d
LeonardParker5 <leonard.parker5@gmail.com>
2023-03-29 16:13:56 -0400

    Added gbp to usd conversion

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::fs;
 
+//const USD_TO_GBP: f64 = 0.71932;
+const GBP_TO_USD: f64 = 1.38910;
+
 fn main() {
     let contents = fs::read_to_string("file.txt")
         .expect("Something went wrong reading the file");
@@ -15,4 +18,7 @@ fn main() {
     }
 
     println!("Sum of pound amounts: £{:.2}", sum);
+
+    let usd: f64 = sum * GBP_TO_USD;
+    println!("${:.2} is the equivalent of £{:.2}", usd, sum);
 }
